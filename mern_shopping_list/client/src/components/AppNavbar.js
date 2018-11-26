288be2b3039ec7ab993090e6d91a8f8eda6ce635
Daniel S <danielksstone@gmail.com>
2018-11-26 09:49:08 -0800

    created client folder and navbar with react

---
+++ mern_shopping_list/client/src/components/AppNavbar.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+    // constructor (props) {
+    //     super(props);
+    //     this.state = {
+    //         isOpen: false
+    //     }
+    // since binding is not used for the toggle do not need a constructor
+
+//        this.toggle = this.toggle.bind(this);
+//          work around for binding toggle to this is a fat arrow function in the toggle function
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return(
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">
+                        Shopping List
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}></NavbarToggler>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/codedaniels">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+    }
+}
+
+
+
+export default AppNavbar;
