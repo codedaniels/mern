881e85c1d9437431ade9b9af3cf3b39a0f5f49b9
Daniel S <danielksstone@gmail.com>
2018-11-25 17:53:08 -0800

    added delete

--- mern_shopping_list/routes/api/items.js
+++ mern_shopping_list/routes/api/items.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 // @route   POST api/items
-// @desc    Create a Post
+// @desc    Create an Item
 // @access  Public
 router.post('/', (req, res) => {
     const newItem = new Item({
@@ -24,4 +24,13 @@ router.post('/', (req, res) => {
     newItem.save().then(item => res.json(item));
 });
 
+// @route   DELETE api/items
+// @desc    Delete a Item
+// @access  Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true}))) 
+})
+    .catch(err => res.status(404).json({success:false}));
+    
 module.exports = router;
