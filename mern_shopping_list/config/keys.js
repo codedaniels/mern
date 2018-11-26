e1808a1915f88c3bb8e65e4717c59a4d59eb4d50
Daniel S <danielksstone@gmail.com>
2018-11-25 16:24:42 -0800

    added keys and config for database mongo and mongoose with port info

---
+++ mern_shopping_list/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://daniel:daniel123@ds153198.mlab.com:53198/mern_shopping'
+}
--- mern_shopping_list/server.js
+++ mern_shopping_list/server.js
@@ -2,3 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+// bodyParser Middleware
+app.use(bodyParser.json());
+
+// MongoDB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
