8c2802c68c48ebb908b809d6dc7f24839e9f33ee
Daniel S <danielksstone@gmail.com>
2018-11-28 16:31:39 -0800

    added import info on app.js file

--- mern_shopping_list/client/src/App.js
+++ mern_shopping_list/client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList'
-;
+import ShoppingList from './components/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
