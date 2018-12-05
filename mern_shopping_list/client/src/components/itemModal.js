98a2b847df68cebf112728d166fd37ce6d6eb13b
Daniel S <danielksstone@gmail.com>
2018-12-05 13:37:00 -0800

    more add stuff in itemModal.js file

--- mern_shopping_list/client/src/components/itemModal.js
+++ mern_shopping_list/client/src/components/itemModal.js
@@ -9,4 +9,11 @@ import {
     Label,
     Input
 } from 'reactstrap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
 
+class ItemModal extends Component {
+    
+}
+
+export default connect()(ItemModal);
